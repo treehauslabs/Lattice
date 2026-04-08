bb157b6b36996140ed0dad484815b6266f86c4b1
pumperknickle <pumperknickle@users.noreply.github.com>
2026-04-07 22:23:04 -0500

    Point cashew dependency to remote 1.3.0 (Volume support)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
-        .package(path: "../cashew"),
+        .package(url: "https://github.com/treehauslabs/cashew.git", from: "1.3.0"),
         .package(url: "https://github.com/treehauslabs/UInt256.git", from: "1.0.0"),
         .package(url: "https://github.com/swift-libp2p/swift-cid.git", from: "0.0.1"),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
